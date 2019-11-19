755bcf6939a8fd0fa24f3b21b8b3d7681b565c8c
foile <foile@mail.ru>
2019-11-19 09:00:07 +0300

    hoc

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import films from './mocks/films';
 
 
 const init = () => {
-  const store = createStore(reducer, {genre: `All genres`, films, allFilms: films});
+  const store = createStore(reducer,
+      {genre: `All genres`, films, allFilms: films},
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  );
   ReactDOM.render(<Provider store={store}>
     <App />
   </Provider>,
